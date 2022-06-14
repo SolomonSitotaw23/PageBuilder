98deda3bf27318caa6976d970c9f99fb516c6f5d
solomonsitotaw23 <solomonsitotaw9@gmail.com>
2022-06-14 16:11:12 +0300

    adding preview single page and custom button components for delete and edit the page

--- src/components/PagesPreview.vue
+++ src/components/PagesPreview.vue
@@ -4,75 +4,82 @@ import { useQuery } from "@vue/apollo-composable";
 
 // !importing data
 
-const CountriesQuery = gql`
-  query Pages {
-    pages {
+const PagesQuery = gql`
+  query {
+    getAllPages {
       id
-      slug
-      lang
       title
-      published
-      content {
-        ... on ImageBlock {
-          url
-          id
-        }
-        ... on TextBlock {
-          id
-          text
-        }
-      }
+      description
+      image_url
     }
   }
 `;
 const press_me = () => {
+  error;
   return alert();
 };
-let count = 0;
-const { result, loading, error } = useQuery(CountriesQuery);
-let indicator = 0;
+
+const { result, loading, error } = useQuery(PagesQuery);
 </script>
 
 <template>
   <div>
-    <div class="grid grid-cols-2 gap-4 p-12 page-preview">
+    <div class="grid grid-cols-3 gap-8 p-12 page-preview">
+      <!-- ________________ -->
       <p v-if="error">Something went wrong...</p>
-
-      <!-- Content Starts -->
-
-      <p v-if="loading" class="text-center self-center w-full loading">
-        Loading...
-      </p>
-      <div
-        v-else
-        v-for="page in result.pages"
-        :key="page.id"
-        class="flex flex-col items-center overflow-hidden mt-5 last-of-type:mb-12 cursor-pointer"
-      >
+      <p v-if="loading" class="text-center self-center w-full loading">Loading...</p>
+      <div v-else v-for="page in result.getAllPages" :key="page.id">
         <!-- content wrapper -->
-        <div v-if="page.id > 10" class="border border-black">
-          {{ (indicator = 0 ? (indicator = 1) : indicator) }}
-
-          <h1 class="text-3xl">{{ page.title }}</h1>
-          <div v-for="content in page.content" :key="content.id" class="">
-            <div class="">
-              <img
-                :src="content.url"
-                alt=""
-                class="w-1/2 float-left relative self-start inline h-full p-5"
-              />
-              <div class="p-4 w-full text-left" v-if="content.text">
-                <p>{{ content.text }}</p>
-              </div>
-            </div>
+        <div
+          class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+        >
+          <a href="#">
+            <img class="rounded-t-lg" :src="page.image_url" alt="" />
+          </a>
+          <div class="p-5">
+            <a href="#">
+              <h5
+                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 capitalize dark:text-white"
+              >
+                {{ page.title }}
+              </h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              {{ page.description }}
+            </p>
+            <!-- router -->
+            <router-link
+              :to="{
+                name: 'page',
+                params: {
+                  id: page.id,
+                  title: page.title,
+                  image_url: page.image_url,
+                  description: page.description,
+                },
+              }"
+              class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Preview
+              <svg
+                class="ml-2 -mr-1 w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </router-link>
+            <!-- router end -->
           </div>
         </div>
-        <!-- content wrapper end -->
+        <!-- content end -->
       </div>
-
-      <!-- content end -->
     </div>
-    <div v-if="indicator === 0"><h1>No content to display</h1></div>
   </div>
 </template>
 <style>
